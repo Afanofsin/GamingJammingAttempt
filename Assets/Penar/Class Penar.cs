8b1825034d7c3a5411a2cf1919aa374aff1eb397
albinolama <rkeeosi@gmail.com>
2025-07-10 22:28:47 +0300

    added GPU bricking feature

--- Assets/Penar/Class Penar.cs
+++ Assets/Penar/Class Penar.cs
@@ -1,20 +1,32 @@
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class ClassPenar : MonoBehaviour
 {
-    public GameObject _gameObject;
-    Rigidbody rb;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject _gameObject;  // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i =0; i < 2; i++) Instantiate(_gameObject, gameObject.transform.position, gameObject.transform.rotation);
+        _gameObject.transform.position += Vector3.forward * 0.5f * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            CreateLagSpike();
+        }
+    }
+    private void CreateLagSpike()
+    {
+        for (int i = 0; i < 1000000000; i++)
+        {
+            Vector3 lag = new Vector3(Mathf.Sin(i), Mathf.Cos(i), Mathf.Tan(i));
+        }
     }
 }
