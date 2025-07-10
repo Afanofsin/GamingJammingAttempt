678d50bff29ead6c0f6a5e4e672e473f9eaf3480
albinolama <rkeeosi@gmail.com>
2025-07-10 22:06:45 +0300

    added ultra penar

--- Assets/Penar/Class Penar.cs
+++ Assets/Penar/Class Penar.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class ClassPenar : MonoBehaviour
 {
+    public GameObject _gameObject;
+    Rigidbody rb;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i =0; i < 2; i++) Instantiate(_gameObject, gameObject.transform.position, gameObject.transform.rotation);
     }
 }
